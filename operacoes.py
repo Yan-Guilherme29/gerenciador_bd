9f70c196bffb3e8f8c3ec2ceae71cfcdd61f3ba2
Yan Guilherme <yanguilherme2927@gmail.com>
2026-09-08 20:49:03 -0300

    refactor: adiciona try/except e conn.close() em todas as operações

--- operacoes.py
+++ operacoes.py
@@ -1,47 +1,96 @@
 from conexao import conectar
 
-def listar_funcionarios():
-    conn = conectar()
-    cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM funcionarios")
-    resultado = cursor.fetchall()
-    return resultado
+def listar_funcionarios():
+    conn = None
+
+    try:
+
+        conn = conectar()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM funcionarios")
+        resultado = cursor.fetchall()
+        return resultado
+
+    except Exception as e:
+        print("Erro ao listar funcionários!")
+
+    finally:
+        if conn:
+            conn.close()
+
 
 def cadastrar_funcionario(nome, cargo, salario, departamento, email):
-    conn = conectar()
-    cursor = conn.cursor()
+    conn = None
 
-    cursor.execute(
-        "INSERT INTO funcionarios (nome , cargo, salario, departamento, email) VALUES (%s, %s, %s, %s, %s)",
-        (nome, cargo, salario, departamento, email)
-    )
+    try:
 
-    conn.commit()
-    print("Dado inserido com sucesso!")
+        conn = conectar()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO funcionarios (nome , cargo, salario, departamento, email) VALUES (%s, %s, %s, %s, %s)",
+            (nome, cargo, salario, departamento, email)
+        )
+
+        conn.commit()
+        print("Funcionário cadastrado com sucesso!")
+
+    except Exception as e:
+        print("Erro ao cadastrar funcionário!")
+
+    finally:
+        if conn:
+            conn.close()
+
 
 def atualizar_salario(id_funcionario, novo_salario):
-    conn = conectar()
-    cursor = conn.cursor()
+    conn = None
 
-    cursor.execute(
-        "UPDATE funcionarios SET salario = %s WHERE id = %s",
-        (novo_salario, id_funcionario)
+    try:
 
-    )
+        conn = conectar()
+        cursor = conn.cursor()
 
-    conn.commit()
-    print("\nSalário alterado com sucesso!")
+        cursor.execute(
+            "UPDATE funcionarios SET salario = %s WHERE id = %s",
+            (novo_salario, id_funcionario)
+
+        )
+
+        conn.commit()
+        print("Salário alterado com sucesso!")
+
+    except Exception as e:
+        print("Erro ao atualizar salário!")
+
+    finally:
+        if conn:
+            conn.close()
+
 
 def deletar_funcionario(id_funcionario):
-    conn = conectar()
-    cursor = conn.cursor()
+    conn = None
 
-    cursor.execute(
-        "DELETE FROM funcionarios WHERE id = %s ",
-        (id_funcionario,)
+    try:
 
-    )
+        conn = conectar()
+        cursor = conn.cursor()
 
-    conn.commit()
-    print("\nFuncionário deletado com sucesso!")
+        cursor.execute(
+            "DELETE FROM funcionarios WHERE id = %s ",
+            (id_funcionario,)
+
+        )
+
+        conn.commit()
+        print("Funcionário deletado com sucesso!")
+
+
+    except Exception as e:
+        print("Erro ao deletar funcionário!")
+
+    finally:
+        if conn:
+            conn.close()
