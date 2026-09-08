229abe5466ebbdbf606028b8c128583efdc8e5a0
Yan Guilherme <yanguilherme2927@gmail.com>
2026-09-08 12:06:54 -0300

    feat: adicionada função de cadastro de funcionários

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
-from operacoes import listar_funcionarios
+from operacoes import listar_funcionarios, cadastrar_funcionario
+
 
 for funcionario in listar_funcionarios():
 
     print(f"\n ID: {funcionario[0]} | Nome: {funcionario[1]} | Cargo: {funcionario[2]} | Salário: {funcionario[3]} | Departamento: {funcionario[4]} | Email: {funcionario[5]}  ")
+
+cadastrar_funcionario("Sarah", "Designer", 5000, "TI", "sarah@gmail.com")
--- operacoes.py
+++ operacoes.py
@@ -9,3 +9,16 @@ def listar_funcionarios():
     cursor.execute("SELECT * FROM funcionarios")
     resultado = cursor.fetchall()
     return resultado
+
+def cadastrar_funcionario(nome, cargo, salario, departamento, email):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO funcionarios (nome , cargo, salario, departamento, email) VALUES (%s, %s, %s, %s, %s)",
+        (nome, cargo, salario, departamento, email)
+    )
+
+    conn.commit()
+    print("Dado inserido com sucesso!")
+
