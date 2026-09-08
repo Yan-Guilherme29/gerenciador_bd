344d7449470360412cee1082af93bf1730e3b164
Yan Guilherme <yanguilherme2927@gmail.com>
2026-09-08 20:36:00 -0300

    feat: adiciona funções de atualizar e deletar funcionnários

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
-from operacoes import listar_funcionarios, cadastrar_funcionario
+from operacoes import *
+
+# cadastrar_funcionario("Sarah", "Designer", 5000, "TI", "sarah@gmail.com")
+
+# atualizar_salario(1, 12000)
+
+# deletar_funcionario(7)
 
 
 for funcionario in listar_funcionarios():
 
     print(f"\n ID: {funcionario[0]} | Nome: {funcionario[1]} | Cargo: {funcionario[2]} | Salário: {funcionario[3]} | Departamento: {funcionario[4]} | Email: {funcionario[5]}  ")
 
-cadastrar_funcionario("Sarah", "Designer", 5000, "TI", "sarah@gmail.com")
--- operacoes.py
+++ operacoes.py
@@ -1,7 +1,5 @@
 from conexao import conectar
 
-
-
 def listar_funcionarios():
     conn = conectar()
     cursor = conn.cursor()
@@ -22,3 +20,28 @@ def cadastrar_funcionario(nome, cargo, salario, departamento, email):
     conn.commit()
     print("Dado inserido com sucesso!")
 
+def atualizar_salario(id_funcionario, novo_salario):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE funcionarios SET salario = %s WHERE id = %s",
+        (novo_salario, id_funcionario)
+
+    )
+
+    conn.commit()
+    print("\nSalário alterado com sucesso!")
+
+def deletar_funcionario(id_funcionario):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM funcionarios WHERE id = %s ",
+        (id_funcionario,)
+
+    )
+
+    conn.commit()
+    print("\nFuncionário deletado com sucesso!")
