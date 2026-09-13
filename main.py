7ceba076b51da41ad5f5491b36308fc88a14ff59
Yan Guilherme <yanguilherme2927@gmail.com>
2026-09-12 22:39:14 -0300

    feat: adiciona menu interativo com opções de listagem (funcionários, projetos, tarefas)

--- main.py
+++ main.py
@@ -1,18 +1,56 @@
 from operacoes_funcionarios import *
 from operacoes_projetos import *
+from operacoes_tarefas import *
 
-print("=-" * 50)
-print("Tabela funcionarios: ")
+while True:
 
-for funcionario in listar_funcionarios():
+    print("=-" * 15)
+    print("\nGerenciador de Banco de dados\n")
+    print("=-" * 15)
+    print("1 - Listar funcionários ")
+    print("2 - Listar projetos")
+    print("3 - Listar tarefas")
+    print("=-" * 15)
+    print("4 - Cadastrar funcionário")
+    print("5 - Cadastrar projeto")
+    print("6 - Cadastrar tarefa")
+    print("=-" * 15)
+    print("7 - Atualizar funcionário")
+    print("8 - Atualizar projeto")
+    print("9 - Atualizar descrição da tarefa")
+    print("10 - Atualizar status da tarefa")
+    print("=-" * 15)
+    print("11 - Deletar funcionário ")
+    print("12 - Deletar projeto")
+    print("13 - Deletar tarefa")
+    print("=-" * 15)
+    print("0 - Sair")
+    print("=-" * 15)
 
-    print(f"\n ID: {funcionario[0]} | Nome: {funcionario[1]} | Cargo: {funcionario[2]} | Salário: {funcionario[3]} | Departamento: {funcionario[4]} | Email: {funcionario[5]}  ")
 
-print("=-"*50)
-print("Tabela projetos: ")
+    escolha = int(input("\nDigite sua escolha: "))
 
-for projeto in listar_projetos():
+    match escolha:
 
-    print(f"\n ID: {projeto[0]} | Título: {projeto[1]} | Funcionário ID: {projeto[2]} ")
+        case 0:
+            print("Sistema finalizado! Até a próxima!")
+            break
 
-print("=-"*50)
+        case 1:
+            for funcionario in listar_funcionarios():
+                print("=-" *15 )
+                print(
+                    f"\n ID: {funcionario[0]} | Nome: {funcionario[1]} | Cargo: {funcionario[2]} | Salário: {funcionario[3]} | Departamento: {funcionario[4]} | Email: {funcionario[5]}"
+                )
+
+        case 2:
+            for projeto in listar_projetos():
+                print(
+                    f"\n ID: {projeto[0]} | Título: {projeto[1]} | Funcionário ID: {projeto[2]} "
+                )
+
+        case 3:
+            for tarefa in listar_tarefas():
+                print(
+                    f"\n ID: {tarefa[0]} | Descrição: {tarefa[1]} | Concluída: {tarefa[2]} | Projeto ID: {tarefa[3]}  "
+                )
